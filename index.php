e5245dfbb19ca1ef5f1d46e214af60b6a9157001
bleyvamoo <blancafer_leyva@hotmail.com>
2018-08-06 14:23:32 -0400

    hola

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Airline</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <meta name="viewport" content="witdh=device, initial-scale=1, user-scalable=no">
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  <h1>Hola</h1>
+<nav class="navbar navbar-default navbar-fixed-top">
+<div class="container">
+  <a href="index.php" class="navbar - brand">Wedding Destinations</a>
+<ul class="nav navbar-nav">
+<li class="dropdown">
+  <a href="a" class="dropdown-toggle" data-toggle="dropdown">About Us<span class="caret"><span></a>
+    <ul class="dropdown-menu" role="menu">
+
+    </ul>
+</li>
+</ul>
+</div>
+</nav>
+</body>
+
+
+</html>
